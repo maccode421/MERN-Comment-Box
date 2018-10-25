9a496bcc741c464df6c2e575d6f74cdb3636e637
macc <maccavalli@yahoo.com>
2018-10-25 19:38:12 -0400

    Add and modify files

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CommentBox from '../component/CommentBox';
+import CommentBox from './components/CommentBox';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<CommentBox />, document.getElementById('root'));
