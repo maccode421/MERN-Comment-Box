5691f6c17dafd7ed5d98c167ec942b655f06aada
macc <maccavalli@yahoo.com>
2018-10-29 19:18:32 -0400

    Add backend file

---
+++ backend/keys.js
@@ -0,0 +1,9 @@
+// const keys = {
+//   dbUri: process.env.DB_URI 
+// };
+
+// export const getKey = key => keys[key];
+
+module.exports = {
+  dbURI: 'mongodb://muse:iamamused7@ds243963.mlab.com:43963/mern-commentbox'
+};
