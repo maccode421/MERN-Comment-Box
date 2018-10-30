2fcc2924c94d61e207a2ff4d9b18dc72a5ffc9fe
macc <maccavalli@yahoo.com>
2018-10-30 13:55:22 -0400

    Add new file and update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const comment = require('./models/comment');
 
 // create our instances
 const app = express();
@@ -24,6 +25,32 @@ app.use(logger('dev'));
 // now we can set the route path & initialize the API
 router.get('/', (req, res) => {
   res.json({ message: 'Hello, World!' });
+});
+
+router.get('/comments', (req, res) => {
+  Comment.find((err, comments) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: comments });
+  });
+});
+
+router.post('/comments', (req, res) => {
+  const comment = new Comment();
+  // body parser lets us use the req.body
+  const { author, text } = req.body;
+  if (!author || !text) {
+    // we should throw an error. we can do this check on the front end
+    return res.json({
+      success: false,
+      error: 'You must provide an author and comment'
+    });
+  }
+  comment.author = author;
+  comment.text = text;
+  comment.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
 });
 
 // Use our router configuration when we call /api
