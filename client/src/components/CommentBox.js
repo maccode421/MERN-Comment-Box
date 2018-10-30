9918fae405b965a80d8ece6f84f504686fb03034
macc <maccavalli@yahoo.com>
2018-10-30 14:25:50 -0400

    Fetch data from server and set state

--- client/src/components/CommentBox.js
+++ client/src/components/CommentBox.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
+import 'whatwg-fetch';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
 import DATA from '../data';
-import '../CommentBox.css';
+import './CommentBox.css';
 
 class CommentBox extends Component {
   constructor() {
     super();
     this.state = {
       data: [],
+      error: null,
+      author: '',
+      text: ''
     };
+    this.pollInterval = null;
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    if (!this.pollInterval) {
+      this.pollInterval = setInterval(this.loadCommentsFromServer, 2000);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.pollInterval) clearInterval(this.pollInterval);
+    this.pollInterval = null;
+  }
+
+  loadCommentsFromServer = () => {
+    // fetch returns a promise
+    fetch('/api/comments/')
+      .then(data => data.json())
+      .then((res) => {
+        if (!res.success) this.setState({ error: res.error });
+        else this.setState({ data: res.data };)
+      });
   }
 
   render() {
@@ -17,11 +44,12 @@ class CommentBox extends Component {
       <div className="container">
         <div className="comments">
           <h2>Comments:</h2>
-          <CommentList data={DATA} />
+          <CommentList data={this.state.data} />
         </div>
         <div className="form">
-          <CommentForm />
+          <CommentForm author={this.state.author} text={this.state.text} />
         </div>
+        {this.state.error && <p>{this.state.error}</p>}
       </div>
     );
   }
