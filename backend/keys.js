183253df467c1cf782d84c6ff7d2d91a7ccbc8bd
macc <maccavalli@yahoo.com>
2018-10-31 18:00:48 -0400

    Minor fix

--- backend/keys.js
+++
@@ -1,9 +0,0 @@
-// const keys = {
-//   dbUri: process.env.DB_URI 
-// };
-
-// export const getKey = key => keys[key];
-
-module.exports = {
-  dbURI: 'mongodb://muse:iamamused7@ds243963.mlab.com:43963/mern-commentbox'
-};
